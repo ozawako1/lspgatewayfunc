8d994cd19d0442435f31fb2ac4e76b4c5e87d55a
ozawako1 <ozawako1@gmail.com>
2018-09-08 07:05:47 +0900

    simplify

--- HttpTriggerJS/webserviceaz.js
+++ HttpTriggerJS/webserviceaz.js
@@ -1,5 +1,21 @@
-var myutil = require('./util.js');
-//require("request");
+
+const my_config = require("../conf/lspgateway.js");
+/* FORMAT of conf/lspgateway.js 
+var config = {};
+config.chatwork = {
+    "token": "xxxxx"
+};
+config.sqlserver = {
+    "userName": "xxx",
+    "password": "xxx",
+    "server": "xxx.database.windows.net",
+    "options":{
+        "encrpyt": true,
+        "database": "xxx"
+    }
+};
+module.exports = config;
+*/
 
 const TO_REG = /\[To:[0-9]*\].*\n/;
 
@@ -40,6 +56,50 @@ class CWebServiceAz {
     }
 }
 
+function format_msg(jdata) {
+    /*
+    {
+        "PCName":"13P1-0905a",
+        "LogonUserName":"",
+        "IPAddr":"192.168.120.1",
+        "MACAddr":"b8e8561206da",
+        "OSVersion":"macOS 10.13.3 (17D102)",
+        "Platform":"",
+        "InvInfoUpdateTime":"2018-03-23T08:37:00.000Z",
+        "MRVersion":"A9000"
+    }
+    */
+    msg = "";
+
+    var resultcount = Object.keys(jdata).length;
+
+    if (resultcount === 0) {
+        msg += "Sorry, i could not find it ...\r\n";
+    } else {
+
+        msg += 'Hi, did you mean ... ?  (nod)';
+        msg += "[info]";
+        if (jdata.length === 0) {
+            msg += "not found.";
+        } else {
+            msg += "コンピューター名: "  + jdata["PCName"] + "\r\n";
+            msg += "ログオンユーザー名: "    + jdata["LogonUserName"] + "\r\n";
+            msg += "IPアドレス: "    + jdata["IPAddr"] + "\r\n";
+            msg += "MACアドレス: "   + jdata["MACAddr"] + "\r\n";
+
+            msg += "OS Version: "    + jdata["OSVersion"] + "\r\n";
+            msg += "32bit/64bit: "   + jdata["Platform"] + "\r\n";
+            msg += "最終更新日時: "  + jdata["InvInfoUpdateTime"] + "\r\n";
+            msg += "MR Version: "    + jdata["MRVersion"] + "\r\n";
+        }
+
+        msg += "[/info]";
+    }
+    msg += "Running On Azure Functions.";
+    return msg;
+};
+
+
 class CWebServiceChatwork extends CWebServiceAz {
     constructor (req) {
         super(req);
@@ -50,12 +110,12 @@ class CWebServiceChatwork extends CWebServiceAz {
         this.roomid = req.body.webhook_event.room_id;
         this.fromid = req.body.webhook_event.account_id;
         this.msgid = req.body.webhook_event.message_id;
-        this.token = myutil.get_chatwork_token();
+        this.token = my_config.chatwork.token;
     }
     
     GetReplyMsg(results) {
         msg = "[rp aid=" + this.fromid + " to=" + this.roomid + "-" + this.msgid +"]" + "\r\n" +
-            myutil.format_msg(results);
+            format_msg(results);
 
         var response = {
             headers: {
